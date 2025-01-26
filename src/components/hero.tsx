88a344bd084b1d9336733a42a4db00282fa2d335
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2025-01-27 02:48:23 +0500

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,58 +1,57 @@
 'use client';
-import React from 'react'
-import '../style/hero.css'
+import React from 'react';
+import '../style/hero.css';
 import { TypeAnimation } from "react-type-animation";
 import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <section className="hero-section" >
-    <div className='hero'>
-     {/* content side */}
-     <div className="content">
-     <span className='hello'>
-     Hello! I&apos;m <br />
-              </span>{" "}
-              
-              <TypeAnimation
-                sequence={[
-                 
-                  &quot;Rahat Bano&quot;,
-                  1000, 
-                  &quot;Graphic Designer&quot;,
-                  1000,
-                &quot;Web Developer&quot;,
-                  1000,
-                  &quot;Frontend Developer&quot;,
-                  1000,
+    <section className="hero-section">
+      <div className='hero'>
+        {/* content side */}
+        <div className="content">
+          <span className='hello'>
+            Hello! I&apos;m <br />
+          </span>
 
-                ]}
-                wrapper="span"
-                speed={60}
-                repeat={Infinity}
-              />
-            
+          <TypeAnimation
+            sequence={[
+              &quot;Rahat Bano&quot;,
+              1000,
+              &quot;Graphic Designer&quot;,
+              1000,
+              &quot;Web Developer&quot;,
+              1000,
+              &quot;Frontend Developer&quot;,
+              1000,
+            ]}
+            wrapper="span"
+            speed={60}
+            repeat={Infinity}
+          />
 
-    <p>
-    I create responsive and high-performance web applications that  deliver seamless user experiences across all devices.
-    </p> 
-    <div className='btn'>
-    <button className='btn1'>Hire Me</button>
-    <button className='btn2'>Download CV</button>
-  </div>
-   </div>
+          <p>
+            I create responsive and high-performance web applications that deliver seamless user experiences across all devices.
+          </p> 
+          <div className='btn'>
+            <button className='btn1'>Hire Me</button>
+            <button className='btn2'>Download CV</button>
+          </div>
+        </div>
 
-
-     {/* image side */}
-     <div className="image">
-      <Image src='/images/profile.jpg' alt='ProfileImg' width={350} height={300}
-      className='design' />
-    
-     </div>
-
-    </div>
+        {/* image side */}
+        <div className="image">
+          <Image
+            src='/images/profile.jpg'
+            alt='ProfileImg'
+            width={350}
+            height={300}
+            className='design'
+          />
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Hero;
